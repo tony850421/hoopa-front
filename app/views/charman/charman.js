0b1c2ad8a9dc6f2efa102936adcdda816742b36a
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2018-06-27 13:55:07 +0800

    adding page featured projects

---
+++ app/views/charman/charman.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('myApp.Charman', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/charman', {
+            templateUrl: 'views/charman/charman.html',
+            controller: 'CharmanCtrl'
+        });
+    }])
+
+    .controller('CharmanCtrl', ['$rootScope', '$scope', '$routeParams', function ($rootScope, $scope, $routeParams) {
+        $scope.projects = [];
+
+        $scope.init = function () {
+
+            var queryProjects = new AV.Query('Project');
+            queryProjects.limit(5);
+            queryProjects.equalTo('isRecommended', true);
+            queryProjects.find().then(function (res) {
+                $scope.projects = [];
+                res.forEach(function (element) {
+                    var mainImage = element.get('image').thumbnailURL(260, 160);
+                    var title = element.get('title');
+                    var amount = element.get('debitAmount');
+                    var id = element.id;
+
+                    $scope.projects.push({
+                        id: id,
+                        mainImage: mainImage,
+                        title: title,
+                        amount: amount
+                    })
+                    $scope.$apply();
+                });
+                $scope.$apply();
+
+            }).catch(function (error) {
+                alert(JSON.stringify(error));
+            });
+        };
+
+        $scope.init();
+    }])
