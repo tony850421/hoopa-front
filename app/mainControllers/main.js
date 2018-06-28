edb294982280b1a4afd0d3f4631c4a9f0499b75e
tony <tony850421@gmx.es>
2018-06-28 16:47:34 +0800

    main

--- app/mainControllers/main.js
+++ app/mainControllers/main.js
@@ -4,9 +4,9 @@ angular.module('myApp.Main', ['ngRoute'])
 
     .controller('MainCtrl', ['$rootScope', '$scope', '$window', '$translate', function ($rootScope, $scope, $window, $translate) {
 
-        // $scope.baseUrl = 'https://hoopa.org/front-end/#!/';
+        $scope.baseUrl = 'https://hoopa.org/front-end/#!/';
         // $scope.baseUrl = 'http://127.0.0.1:8000/#!/';
-        $scope.baseUrl = 'http://192.168.3.113:8000/#!/';
+        // $scope.baseUrl = 'http://192.168.3.113:8000/#!/';
         // $scope.baseUrl = 'http://localhost/hoopa-front/app/#!/';
 
         $scope.languageShort = "English";
